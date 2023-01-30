acd531f095aa03809038217498ad761bcd388a93
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2023-01-30 10:37:08 -0600

    Update __init__.py

--- bagelapp/__init__.py
+++ bagelapp/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'MYKEYSTUFF'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # quiet warning message
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # quiet warning message
 db = SQLAlchemy(app)
 
 
